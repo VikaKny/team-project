6814d2b657b1728da6e3f975a21a99c08f20c637
Anhelina <beisiukangelina@MacBook-Air-Bejsuk.local>
2026-03-27 13:58:06 +0200

    number

--- task.branch.py
+++ task.branch.py
@@ -3,26 +3,60 @@ contacts = []
 while True:
     print("\n1 - Додати контакт")
     print("2 - Показати контакти")
-    print("3 - Вихід")
+    print("3 - Видалити контакт")
+    print("4 - Вихід")
 
     choice = input("Виберіть дію: ")
 
+
     if choice == "1":
+        name = input("Введіть ПІБ: ")
         number = input("Введіть номер телефону: ")
-        contacts.append(number)
+
+        contact = {
+            "name": name,
+            "phone": number
+        }
+
+        contacts.append(contact)
         print("Контакт додано")
+
 
     elif choice == "2":
         print("\nСписок контактів:")
+
         if len(contacts) == 0:
             print("Контактів немає")
         else:
             for i, c in enumerate(contacts, 1):
-                print(i, "-", c)
+                print(f"{i} - {c['name']} : {c['phone']}")
+
 
     elif choice == "3":
+        if len(contacts) == 0:
+            print("Немає що видаляти")
+        else:
+            print("\nСписок контактів:")
+            for i, c in enumerate(contacts, 1):
+                print(f"{i} - {c['name']} : {c['phone']}")
+
+            try:
+                index = int(input("Введіть номер контакту для видалення: ")) - 1
+
+                if 0 <= index < len(contacts):
+                    deleted = contacts.pop(index)
+                    print(f"Видалено: {deleted['name']}")
+                else:
+                    print("Невірний номер")
+
+            except ValueError:
+                print("Введіть число!")
+
+
+    elif choice == "4":
         print("Програма завершена")
         break
+
 
     else:
         print("Невірний вибір")
